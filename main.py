27a568817c3fb283163ae9f108e300c91a73a51f
Aigerim Bekbolatova <aigerim0799@gmail.com>
2022-10-03 14:47:30 +0600

    ПРАКТИЧЕСКАЯ РАБОТА № 1 Создание простейшей консольной программы

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    try:
+        a = int(input('Введите A: '))
+        b = int(input('Введите B: '))
+        x = int(input('Введите X: '))
+        if x < 7:
+            y = x * x + a * a + b * b / (a + b)
+        else:
+            y = x * x * x * (a + b) ** 2
+        print("y = %.1f" % y)
+    except ValueError:
+        print("Неверные входные данные!")
+    except:
+        print("Возникла ошибка!")
+    input("Нажмите Enter для выхода")
+
+
+if __name__ == '__main__':
+    main()
